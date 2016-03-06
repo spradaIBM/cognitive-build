35a6a58eed28238e4501b1a19e847494ae2b4da0
santiago prada <santiago@santiagos-MBP.attlocal.net>
2016-03-06 17:26:39 -0600

    remove unused stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,33 +6,6 @@ $(function() {
     $(window).load(function(){
         $('#page-loader').fadeOut('slow');
     });
-    
-    
-    /*---------------------------------------*/
-    /*  JQUERY FOR PAGE SCROLLING FEATURE
-    /*  requires jQuery Easing plugin
-    /*---------------------------------------*/
-    var pageScroll = function(){
-        $('.page-scroll a').bind('click', function(e){
-            e.preventDefault();
-
-            var $anchor = $(this);
-
-            var offset = $('body').attr('data-offset');
-            
-            if($('.navbar.navbar-fixed-top').hasClass('side-menu') && $(window).width() >= 992){
-                $('body').data('offset', 1);
-                offset = $('body').data('offset');
-            }
-
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - (offset - 1)
-            }, 1500, 'easeInOutExpo');
-            
-            $('.navbar-rj-collapse').collapse('hide');
-        });
-    };
- 
 
     /*---------------------------------------*/
     /*  OWL CAROUSEL
