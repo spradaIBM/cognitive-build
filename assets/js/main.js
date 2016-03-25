322f9ee715eee8de133c588e528b9968b0ebd815
santiago prada <santiago@santiagos-MBP.attlocal.net>
2016-03-25 08:04:41 -0500

    final updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,74 @@
+   
+    
+    /*---------------------------------------*/
+    /*  JQUERY FOR PAGE SCROLLING FEATURE
+    /*  requires jQuery Easing plugin
+    /*---------------------------------------*/
+    var pageScroll = function(){
+        $('.page-scroll a').bind('click', function(e){
+            e.preventDefault();
+
+            var $anchor = $(this);
+
+            var offset = $('body').attr('data-offset');
+            
+            if($('.navbar.navbar-fixed-top').hasClass('side-menu') && $(window).width() >= 992){
+                $('body').data('offset', 1);
+                offset = $('body').data('offset');
+            }
+
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - (offset - 1)
+            }, 1500, 'easeInOutExpo');
+            
+            $('.navbar-rj-collapse').collapse('hide');
+        });
+    };
+    
+    
+    /*---------------------------------------*/
+    /*  STICKY NAVBAR
+    /*---------------------------------------*/
+    $('.navbar.navbar-fixed-top').sticky({topSpacing: 0});
+    
+    var stickySideMenu = function(){
+        var navbar = $('.navbar.navbar-fixed-top.side-menu');
+        
+        if ($(window).width() >= 992) {        
+            navbar.unstick();
+        }
+        else
+        {
+            navbar.unstick();
+            navbar.sticky({topSpacing: 0});
+        }
+    };
+    
+    pageScroll();
+    stickySideMenu();
+    
+    $(window).smartresize(function(){
+        pageScroll();
+        stickySideMenu();
+    });
+    
+    $('.navbar-trigger-open').click(function(e) {
+        e.preventDefault();
+        $('.navbar.side-menu').toggleClass('active');
+        $('body.push.push-left').toggleClass('pushed-left');
+        $('body.push.push-right').toggleClass('pushed-right');
+    });
+
+    $('.navbar-trigger-close').click(function(e) {
+        e.preventDefault();
+        $('.navbar.side-menu').toggleClass('active');
+        $('body.push.push-left').toggleClass('pushed-left');
+        $('body.push.push-right').toggleClass('pushed-right');
+    });
+    
+
+
+
 $(function() {
 
     /*---------------------------------------*/
